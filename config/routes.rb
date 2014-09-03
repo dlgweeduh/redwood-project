4d173fb3fe1c07ec1598d0fd14e9a66f6c6ec922
davidlguida@gmail.com <davidlguida@gmail.com>
2014-09-03 09:58:27 -0400

    add a Static Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+# StaticPagesController is a Ruby class, but because it inherits from ApplicationController the behavior of its methods is specific to Rails: when visiting the URL /static_pages/home, Rails looks in the Static Pages controller and executes the code in the home action, and then renders the view (the V in MVC from Section 1.3.3) corresponding to the action. In the present case, the home action is empty, so all visiting /static_pages/home does is render the view.
 Rails.application.routes.draw do
   get 'static_pages/home'
 
