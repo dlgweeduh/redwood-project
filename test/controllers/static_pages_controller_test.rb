21c1aedec400bd940088d2c1ee1813b86efb11d9
davidlguida@gmail.com <davidlguida@gmail.com>
2014-09-03 12:09:32 -0400

    refactor titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,35 +1,39 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end 
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App | Home"
+    assert_select "title", "#{@base_title}"
   end
 
-  test "should get about" do
+  test "should get about us" do
     get :about
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App | About Us"    
+    assert_select "title", "#{@base_title} | About Us"
   end
 
   test "should get school" do
     get :school
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App | School"    
+    assert_select "title", "#{@base_title} | School"
   end
 
   test "should get business" do
     get :business
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App | Business"    
-  end
+    assert_select "title", "#{@base_title} | Business"
+  end 
 
   test "should get individual" do
     get :individual
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App | Individual"    
-  end
-
+    assert_select "title", "#{@base_title} | Individual"
+  end   
 
 end
