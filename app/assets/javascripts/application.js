a2fa0bcd491bff7699c0a4f9d76153207d8b2ad9
davidlguida@gmail.com <davidlguida@gmail.com>
2014-12-02 03:32:36 -0500

    massive overhaul

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
+//= require foundation
 //= require turbolinks
 //= require_tree .
+
+$(function(){ $(document).foundation(); });
+
